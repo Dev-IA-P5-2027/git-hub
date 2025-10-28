1af617fd95ecac0b9acb72f5d588221d0ba22909
ZarcDmC01 <jessesteven26@gmail.com>
2025-10-28 15:10:04 +0100

    cc

---
+++ a = 1.py
@@ -0,0 +1,42 @@
+
+# def sum(a,b):
+#     return a+b
+
+# a = int(input("ajoutez un nombre a"))
+# b = int(input("ajoutez un nombre b"))
+
+# print(sum(5,6))
+
+# a = int(input("ajoutez un nombre a"))
+# b = int(input("ajoutez un nombre b"))
+# print(a+b)
+
+# for char in my hub
+# a = int(input("ajoutez un nombre a"))
+# b = int(input("ajoutez un nombre b"))
+# if a > b : 
+#     print("a est supérieur à  b")
+# else :
+#     print("a est plus petit que b")
+
+# print("Bonjour")
+# print("Bonjour")
+# print("Bonjour"*3)
+
+# int("a")
+# a = chr(98)
+# print(a)
+
+mot_de_passe = input("rentrez le mots de passe")
+
+if any(mot_de_passe) == False :
+    print("Il manque une majusucle")
+
+if mot_de_passe != "Hello!!!":
+    print("Mots de passe incorect")
+else :
+    print("Mots de passe corecte")
+
+
+
+
