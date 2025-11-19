65c022c4bfe9b0020e2dfc62271dc769e9f747d5
ZarcDmC01 <jessesteven26@gmail.com>
2025-11-19 10:26:00 +0100

    pisc

--- manouvelleliste.py
+++ manouvelleliste.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 
 liste = []
@@ -17,18 +18,24 @@ def afficher():
 #     with open('liste.csv', 'w', newline='', encoding='utf-8') as fichier_csv:
 #     liste = csv.writer(fichier_csv)
 #     liste.writerows(lignes)
-import csv
 
 def sauvegarder(liste):
-    with open('liste.csv', 'w', newline='', encoding='utf-8') as fichier_csv:
-        writer = csv.writer(fichier_csv)
-        writer.writerows(liste)
+    with open(r"C:\Users\jesse.richard\Desktop\git-hub\liste.json", "w", encoding="utf-8") as f:
+        json.dump(liste, f, ensure_ascii=False, indent=4)
 
-
-ma_liste("abricot")
-print(liste)
-sauvegarder(liste)
-
-
-print("Bonjour choisisser parmis les solution suivante :")
-print("1.ajouter un objet à ma liste \n 2.enlever un objet de ma liste \n 3.afficher ma liste \n 4.sauveguer ma liste")
+while True:
+    print("Bonjour choisisser parmis les solution suivante :")
+    print("1.ajouter un objet à ma liste \n 2.enlever un objet de ma liste \n 3.afficher ma liste \n 4.sauveguer ma liste \n 5.quitter" )
+    n = int(input("Entre ta solution"))
+    if n == 1:
+        objet = input("Entre le nom du produit que tu veux ajoutez")
+        ma_liste(objet)
+    if n == 2:
+        objet = input("Entre le nom du produit que tu veux retirer")
+        enlever(objet)
+    if n == 3:
+        afficher()
+    if n == 4:
+        sauvegarder(liste)
+    if n == 5:
+        break
